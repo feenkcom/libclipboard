8e5c94e16d0eafbdb4bcc68cb3775837f7db48c7
Aliaksei Syrel <alex.syrel@gmail.com>
2019-10-28 20:01:29 +0100

    use boxer

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
 #![allow(non_snake_case)]
 
 extern crate clipboard;
-extern crate libc;
 extern crate boxer;
 
 use boxer::CBox;
 
-use std::os::raw::c_char;
-
 use clipboard::ClipboardProvider;
 use clipboard::ClipboardContext;
+use boxer::string::BoxerString;
 
 #[no_mangle]
 pub fn clipboard_test() -> bool {
@@ -23,11 +21,11 @@ pub fn clipboard_create_clipboard_context() -> *mut ClipboardContext {
 
 #[no_mangle]
 pub fn clipboard_destroy_clipboard_context(_ptr: *mut ClipboardContext) {
-    CBox::from_raw(_ptr);
+    CBox::drop(_ptr);
 }
 
 #[no_mangle]
-pub fn clipboard_get_contents(_ptr_context: *mut ClipboardContext) -> *mut c_char {
+pub fn clipboard_get_contents(_ptr_context: *mut ClipboardContext, _ptr_contents: *mut BoxerString) {
     CBox::with_raw(_ptr_context, |context| {
         let contents_string: String = match context.get_contents() {
             Ok(_string) => { _string },
@@ -35,20 +33,14 @@ pub fn clipboard_get_contents(_ptr_context: *mut ClipboardContext) -> *mut c_cha
                 eprintln!("[Clipboard] Error while getting a content {:?}", e);
                 "".to_string() }
         };
-        CBox::to_chars(contents_string)
+        CBox::with_raw(_ptr_contents, |contents| contents.set_string(contents_string))
     })
 }
 
 #[no_mangle]
-pub fn clipboard_free_contents(_ptr_contents: *mut c_char) {
-    CBox::free_chars(_ptr_contents)
-}
-
-#[no_mangle]
-pub fn clipboard_set_contents(_ptr_context: *mut ClipboardContext, _ptr_contents: *const c_char) {
-    CBox::with_raw(_ptr_context, |context| {
-        let contents_string = CBox::to_string(_ptr_contents);
-        match context.set_contents(contents_string) {
+pub fn clipboard_set_contents(_ptr_context: *mut ClipboardContext, _ptr_contents: *mut BoxerString) {
+    CBox::with_two_raw(_ptr_context, _ptr_contents,|context, contents| {
+        match context.set_contents(contents.to_string()) {
             Ok(_) => {},
             Err(e) => { eprintln!("[Clipboard] Error while setting a content {:?}", e) },
         }
