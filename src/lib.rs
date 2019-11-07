c9852dd5d56be0c21156e68e505c2bed12c37b4d
Aliaksei Syrel <alex.syrel@gmail.com>
2019-11-07 19:22:40 +0100

    wrap context in ValueBox

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use boxer::CBox;
 use clipboard::ClipboardProvider;
 use clipboard::ClipboardContext;
 use boxer::string::BoxerString;
+use boxer::boxes::{ValueBox, ValueBoxPointer};
 
 #[no_mangle]
 pub fn clipboard_test() -> bool {
@@ -15,34 +16,41 @@ pub fn clipboard_test() -> bool {
 }
 
 #[no_mangle]
-pub fn clipboard_create_clipboard_context() -> *mut ClipboardContext {
-    CBox::into_raw(ClipboardProvider::new().unwrap())
+pub fn clipboard_create_clipboard_context() -> *mut ValueBox<ClipboardContext> {
+    match ClipboardProvider::new() {
+        Ok(clipboard_provider) => { ValueBox::new(clipboard_provider).into_raw() },
+        Err(_) => { std::ptr::null_mut() },
+    }
 }
 
 #[no_mangle]
-pub fn clipboard_destroy_clipboard_context(_ptr: *mut ClipboardContext) {
-    CBox::drop(_ptr);
+pub fn clipboard_destroy_clipboard_context(_ptr: *mut ValueBox<ClipboardContext>) {
+    _ptr.drop()
 }
 
 #[no_mangle]
-pub fn clipboard_get_contents(_ptr_context: *mut ClipboardContext, _ptr_contents: *mut BoxerString) {
-    CBox::with_raw(_ptr_context, |context| {
-        let contents_string: String = match context.get_contents() {
-            Ok(_string) => { _string },
-            Err(e) => {
-                eprintln!("[Clipboard] Error while getting a content {:?}", e);
-                "".to_string() }
-        };
-        CBox::with_raw(_ptr_contents, |contents| contents.set_string(contents_string))
-    })
+pub fn clipboard_get_contents(_ptr_context: *mut ValueBox<ClipboardContext>, _ptr_contents: *mut BoxerString) {
+    _ptr_context.with_not_null(|context| {
+        CBox::with_raw(_ptr_contents, |contents| {
+            let contents_string: String = match context.get_contents() {
+                Ok(_string) => { _string },
+                Err(e) => {
+                    eprintln!("[Clipboard] Error while getting a content {:?}", e);
+                    "".to_string() }
+            };
+            contents.set_string(contents_string)
+        })
+    });
 }
 
 #[no_mangle]
-pub fn clipboard_set_contents(_ptr_context: *mut ClipboardContext, _ptr_contents: *mut BoxerString) {
-    CBox::with_two_raw(_ptr_context, _ptr_contents,|context, contents| {
-        match context.set_contents(contents.to_string()) {
+pub fn clipboard_set_contents(_ptr_context: *mut ValueBox<ClipboardContext>, _ptr_contents: *mut BoxerString) {
+    _ptr_context.with_not_null(|context| {
+        CBox::with_raw(_ptr_contents, |contents| {
+            match context.set_contents(contents.to_string()) {
             Ok(_) => {},
             Err(e) => { eprintln!("[Clipboard] Error while setting a content {:?}", e) },
         }
+        })
     });
 }
