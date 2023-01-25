bd7a3ad39a04ae23216f9addc9e9812408f0f51f
Aliaksei Syrel <alex.syrel@gmail.com>
2023-01-25 14:17:52 +0100

    include value-box-ffi and phlow-ffi as part of the library

---
+++ library/src/clipboard_context.rs
@@ -0,0 +1,51 @@
+use copypasta::ClipboardContext;
+use copypasta::ClipboardProvider;
+use string_box::StringBox;
+use value_box::{BoxerError, ReturnBoxerResult, ValueBox, ValueBoxPointer};
+
+#[no_mangle]
+pub fn clipboard_create_clipboard_context() -> *mut ValueBox<ClipboardContext> {
+    match ClipboardContext::new() {
+        Ok(clipboard_provider) => ValueBox::new(clipboard_provider).into_raw(),
+        Err(_) => std::ptr::null_mut(),
+    }
+}
+
+#[no_mangle]
+pub fn clipboard_destroy_clipboard_context(ptr: *mut ValueBox<ClipboardContext>) {
+    ptr.release();
+}
+
+#[no_mangle]
+pub fn clipboard_get_contents(
+    context: *mut ValueBox<ClipboardContext>,
+    contents: *mut ValueBox<StringBox>,
+) {
+    contents
+        .with_mut(|contents| {
+            context
+                .with_mut(|context| {
+                    context
+                        .get_contents()
+                        .map_err(|error| BoxerError::AnyError(error))
+                })
+                .map(|string| contents.set_string(string))
+        })
+        .log();
+}
+
+#[no_mangle]
+pub fn clipboard_set_contents(
+    context: *mut ValueBox<ClipboardContext>,
+    contents: *mut ValueBox<StringBox>,
+) {
+    contents
+        .with_ref(|contents| {
+            context.with_mut(|context| {
+                context
+                    .set_contents(contents.to_string())
+                    .map_err(|error| BoxerError::AnyError(error))
+            })
+        })
+        .log();
+}
--- library/src/lib.rs
+++ library/src/lib.rs
@@ -1,59 +1,22 @@
 #![allow(non_snake_case)]
-extern crate copypasta;
 
-use copypasta::ClipboardContext;
-use copypasta::ClipboardProvider;
-use string_box::StringBox;
-use value_box::{BoxerError, ReturnBoxerResult, ValueBox, ValueBoxPointer};
+#[macro_use]
+extern crate phlow;
+extern crate phlow_extensions;
+extern crate phlow_ffi;
+extern crate value_box_ffi;
+
+use phlow_extensions::CoreExtensions;
+// Re-export everything from the `value_box_ffi` and `phlow_ffi` in order to tell Rust to include
+// the corresponding `no_mangle` functions.
+pub use phlow_ffi::*;
+pub use value_box_ffi::*;
+
+mod clipboard_context;
+
+import_extensions!(CoreExtensions);
 
 #[no_mangle]
 pub fn clipboard_test() -> bool {
     return true;
-}
-
-#[no_mangle]
-pub fn clipboard_create_clipboard_context() -> *mut ValueBox<ClipboardContext> {
-    match ClipboardContext::new() {
-        Ok(clipboard_provider) => ValueBox::new(clipboard_provider).into_raw(),
-        Err(_) => std::ptr::null_mut(),
-    }
-}
-
-#[no_mangle]
-pub fn clipboard_destroy_clipboard_context(ptr: *mut ValueBox<ClipboardContext>) {
-    ptr.release();
-}
-
-#[no_mangle]
-pub fn clipboard_get_contents(
-    context: *mut ValueBox<ClipboardContext>,
-    contents: *mut ValueBox<StringBox>,
-) {
-    contents
-        .with_mut(|contents| {
-            context
-                .with_mut(|context| {
-                    context
-                        .get_contents()
-                        .map_err(|error| BoxerError::AnyError(error))
-                })
-                .map(|string| contents.set_string(string))
-        })
-        .log();
-}
-
-#[no_mangle]
-pub fn clipboard_set_contents(
-    context: *mut ValueBox<ClipboardContext>,
-    contents: *mut ValueBox<StringBox>,
-) {
-    contents
-        .with_ref(|contents| {
-            context.with_mut(|context| {
-                context
-                    .set_contents(contents.to_string())
-                    .map_err(|error| BoxerError::AnyError(error))
-            })
-        })
-        .log();
 }
